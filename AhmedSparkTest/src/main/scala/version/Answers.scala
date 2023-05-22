85560caaeedf1c55da349aa6cdd627664506cbea
AhmedSZaman <S3813842@student.rmit.edu.au>
2023-05-22 21:59:25 +1000

    Cleaned up directory

--- AhmedSparkTest/src/main/scala/version/Answers.scala
+++ AhmedSparkTest/src/main/scala/version/Answers.scala
@@ -18,8 +18,8 @@ object Answers {
 
     println("STARTING QUESTION 1")
     import spark.implicits._
-    val accountDataFrame = helperFunc.loadDataSet[accountData]("src/main/resources/data/sampleAccountData.txt")
-    val customerDataFrame = helperFunc.loadDataSet[customerData]("src/main/resources/data/sampleCustomerData.txt")
+    val accountDataFrame = helperFunc.loadDataSet[accountData]("src/main/resources/data/input/account_data.txt")
+    val customerDataFrame = helperFunc.loadDataSet[customerData]("src/main/resources/data/input/customer_data.txt")
     val CustomerAccountOutput = helperFunc.createCustomerAccountDataSet(spark, accountDataFrame, customerDataFrame)
     CustomerAccountOutput.show(truncate = false)
     helperFunc.saveDataSet("src/main/resources/data/output/Question1", CustomerAccountOutput)(Encoders.product[customerAccountData])
@@ -27,7 +27,7 @@ object Answers {
 
     println("STARTING QUESTION 2")
     val customerAccountDataSet = spark.read.parquet("src/main/resources/data/output/Question1").as[customerAccountData]
-    val addressDataSet = helperFunc.loadDataSet[addressData]("src/main/resources/data/address_data2.txt")
+    val addressDataSet = helperFunc.loadDataSet[addressData]("src/main/resources/data/input/address_data.txt")
     val customerDocumentDataSet = helperFunc.createCustomerDocumentDataSet(spark, addressDataSet, customerAccountDataSet)
     customerDocumentDataSet.show(truncate = false)
     helperFunc.saveDataSet("src/main/resources/data/output/Question2", customerDocumentDataSet)(Encoders.product[customerDocumentData])
