ce3327a0f258a445f4095389bd546fa7f02c2466
AhmedSZaman <S3813842@student.rmit.edu.au>
2023-05-22 22:46:34 +1000

    Finished Unit testing

--- AhmedSparkTest/src/test/scala/questions/SparkTest.scala
+++ AhmedSparkTest/src/test/scala/questions/SparkTest.scala
@@ -1,28 +1,17 @@
 package questions
+import org.apache.log4j.{Level, Logger}
 import org.jetbrains.annotations.TestOnly
 import org.apache.spark.sql.SparkSession
 import org.scalatest._
 import org.scalatest.funsuite.AnyFunSuite
-import version.{accountData, customerData, customerAccountData}
+import version._
 import version.QuestionFunctions
 import org.apache.spark.sql.Encoders._
 
 import scala.collection.mutable
 class SparkTest extends AnyFunSuite with BeforeAndAfterAll {
   val helperFunc = new QuestionFunctions()
-  /*
-  @transient implicit var spark: SparkSession = _
 
-
-  override def beforeAll(): Unit = {
-     spark = SparkSession
-      .builder
-      .appName("Question1")
-      .master("local[*]")
-      .getOrCreate()
-    val sparkSesh: SparkSession = spark
-    import sparkSesh.implicits._
-  }*/
   @transient lazy val spark = SparkSession
     .builder()
     .appName("Quest2ion1")
@@ -30,6 +19,7 @@ class SparkTest extends AnyFunSuite with BeforeAndAfterAll {
     .getOrCreate()
   import spark.implicits._
 
+  Logger.getLogger("org").setLevel(Level.ERROR)
   override def afterAll(): Unit={
       spark.stop()
   }
@@ -42,12 +32,36 @@ class SparkTest extends AnyFunSuite with BeforeAndAfterAll {
   test("Create Customer Account DataSet") {
     val sampleAccountDataFrame = helperFunc.loadDataSet[accountData]("src/test/resources/sampleAccountData.txt")(spark, product[accountData])
     val sampleCustomerDataFrame = helperFunc.loadDataSet[customerData]("src/test/resources/sampleCustomerData.txt")(spark, product[customerData])
-    val sampleCustAccDF = helperFunc.createCustomerAccountDataSet(spark, sampleAccountDataFrame,sampleCustomerDataFrame)
+    val sampleCustAccDF = helperFunc.createCustomerAccountDataSet(spark, sampleAccountDataFrame, sampleCustomerDataFrame)
 
     val expectedSchema = product[customerAccountData].schema
     val actualSchema = sampleCustAccDF.schema
-    assert( expectedSchema.fieldNames.sameElements( actualSchema.fieldNames), "Names should match case class")
-    for (fieldName <- expectedSchema.fieldNames){
+    assert(expectedSchema.fieldNames.sameElements(actualSchema.fieldNames), "Names should match case class")
+    for (fieldName <- expectedSchema.fieldNames) {
+      assert(expectedSchema(fieldName).dataType.typeName == actualSchema(fieldName).dataType.typeName, "Datatypes do not match")
+    }
+  }
+  test("Save Data"){
+    val sampleAccountDataFrame = helperFunc.loadDataSet[accountData]("src/test/resources/sampleAccountData.txt")(spark, product[accountData])
+    val sampleCustomerDataFrame = helperFunc.loadDataSet[customerData]("src/test/resources/sampleCustomerData.txt")(spark, product[customerData])
+    val testData = helperFunc.createCustomerAccountDataSet(spark, sampleAccountDataFrame, sampleCustomerDataFrame)
+    helperFunc.saveDataSet("src/test/resources/sampleCustomerAccountData", testData)(product[customerAccountData])
+    val savedData = spark.read.parquet("src/test/resources/sampleCustomerAccountData").as[customerAccountData]
+
+    for (fieldName <- testData.schema.fieldNames) {
+      assert(testData.schema(fieldName).dataType.typeName == savedData.schema(fieldName).dataType.typeName, "Datatypes do not match")
+    }
+}
+  test("Create Customer Account Document") {
+    val customerAccountDataSet = spark.read.parquet("src/test/resources/sampleCustomerAccountData").as[customerAccountData]
+    val addressDataSet = helperFunc.loadDataSet[addressData]("src/test/resources/sampleAddressData.txt")(spark, product[addressData])
+    val customerDocumentDataSet = helperFunc.createCustomerDocumentDataSet(spark, addressDataSet, customerAccountDataSet)
+
+    val expectedSchema = product[customerDocumentData].schema
+    val actualSchema = customerDocumentDataSet.schema
+
+    assert(expectedSchema.fieldNames.sameElements(actualSchema.fieldNames), "Names should match case class")
+    for (fieldName <- expectedSchema.fieldNames) {
       assert(expectedSchema(fieldName).dataType.typeName == actualSchema(fieldName).dataType.typeName, "Datatypes do not match")
     }
   }
